91560b6ce7d0dc3abbf240e7140e53d6b6c3a1ce
jhamile4 <jhamile.macavilca@tecsup.edu.pe>
2026-09-09 17:03:48 -0500

    Agregar Cliente.cs a Semana1

---
+++ Semana1/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace semana1
+{
+    public class Cliente : Usuario
+    {
+        public double LitrosCargados { get; set; }
+
+        public Cliente(string nombre, double saldo, double litrosCargados)
+            : base(nombre, saldo)
+        {
+            LitrosCargados = litrosCargados;
+        }
+
+        public override void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Saldo: {Saldo}");
+            Console.WriteLine($"Litros cargados: {LitrosCargados}");
+        }
+    }
+}
