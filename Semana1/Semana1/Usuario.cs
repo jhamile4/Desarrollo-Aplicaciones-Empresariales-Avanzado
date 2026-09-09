7bcafa4a6e4230492795d8e185ec2a1297bddecf
jhamile4 <jhamile.macavilca@tecsup.edu.pe>
2026-09-09 17:13:28 -0500

    Add Usuario class with properties and method

---
+++ Semana1/Semana1/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace semana1
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public double Saldo { get; set; }
+
+        public Usuario(string nombre, double saldo)
+        {
+            Nombre = nombre;
+            Saldo = saldo;
+        }
+
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Saldo: {Saldo}");
+        }
+    }
+}
